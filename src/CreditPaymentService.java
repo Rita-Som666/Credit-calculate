b5a366287101d0ad836361b6f299422a9a8f4c92
Tatiana Titova <ritasom666@gmail.com>
2023-11-02 17:36:43 +0400

    initial commit

---
+++ src/CreditPaymentService.java
@@ -0,0 +1,8 @@
+public class CreditPaymentService {
+    public int calculate(int year, double monthlyPercent, int monthlyPayment, int credit) {
+       double percent = 9.99;
+        monthlyPercent = credit * percent / 100;
+        
+
+    }
+}
