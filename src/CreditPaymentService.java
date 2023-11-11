5d9aefb33b9811f2adaf275f127f6321339b45db
Tatiana Titova <ritasom666@gmail.com>
2023-11-11 16:18:51 +0400

    project v2

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,10 +1,10 @@
 public class CreditPaymentService {
-    public double calculate(int loanSum, int numberOfPayments, double percent) {
-        double monthlyPercent = percent/12/100;
-        double monthlyPayment;
-        monthlyPayment = (loanSum * (monthlyPercent * (Math.pow((1 + monthlyPercent), numberOfPayments)) / (Math.pow((1+monthlyPercent), numberOfPayments))));
-        return monthlyPayment;
+    public double calculate(int loanSum, int years, double percent) {
 
-
+        double monthlyPercent = percent / 12 / 100;
+        int numberOfPayments = years * 12;
+        double monthlyPayment = (loanSum * monthlyPercent) /
+                (1 - Math.pow(1 + monthlyPercent, -numberOfPayments));
+        return (int) monthlyPayment;
     }
 }
